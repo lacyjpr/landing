17d44b750e3880b27b3c801a89bacfc8b66f668d
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-03-11 11:50:09 -0700

    Add skeleton header

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <nav className='nav'>
+      <div className='nav__title'>Start Bootstrap</div>
+      <Button variant='primary'>Sign In</Button>
+    </nav>
+  );
+};
+
+export default Header;
