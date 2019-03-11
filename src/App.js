101185486b2a99cd95684e582e8e78a9a73bd48e
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-03-11 11:45:40 -0700

    Create skeleton navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './styles/styles.scss';
+import Header from './components/header';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Header />
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
