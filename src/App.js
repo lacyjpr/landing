8df67ca3f34cc76e7c638502e5604ccc1295201b
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-03-12 12:12:27 -0700

    Style image

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import './styles/styles.scss';
 import Header from './components/header';
+import Home from './components/home';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Header />
+        <Home />
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import blueWater from '../../assets/blue-water.jpg';
+import './Home.scss';
 
 const Home = () => {
-  return <div className='home' />;
+  return (
+    <div className='home'>
+      <img className='home__image' alt='Blue Water' src={blueWater} />
+    </div>
+  );
 };
 
 export default Home;
