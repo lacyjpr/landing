d41216447a2799aed6d9aac5b995921ec8e8ece5
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-03-12 12:20:23 -0700

    Add text to image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,6 +6,10 @@ const Home = () => {
   return (
     <div className='home'>
       <img className='home__image' alt='Blue Water' src={blueWater} />
+      <h2 className='home__text'>
+        Build a landing page for your business or project and generate more
+        leads!
+      </h2>
     </div>
   );
 };
