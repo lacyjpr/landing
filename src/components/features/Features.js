ba9dc2bbdb477ce67a0d03bdc04a7dcd52aa58a5
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-03-31 11:00:40 -0700

    Add initial icons

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faDesktop,
+  faLayerGroup,
+  faCheckCircle,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Features = () => {
-  return <h1>Features Component</h1>;
+  return (
+    <div>
+      <h1>Features Component</h1>
+      <FontAwesomeIcon icon={faDesktop} />
+      <FontAwesomeIcon icon={faLayerGroup} />
+      <FontAwesomeIcon icon={faCheckCircle} />
+    </div>
+  );
 };
 
 export default Features;
