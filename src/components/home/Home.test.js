84f39b3579c4efc83f878362e49119c904b5bb0f
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-03 11:15:45 -0700

    Set up enzyme & write shallow tests

--- src/components/home/Home.test.js
+++ src/components/home/Home.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import Home from './Home';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<Home />', () => {
+  it('renders without crashing', () => {
+    shallow(<Home />);
+  });
 });
