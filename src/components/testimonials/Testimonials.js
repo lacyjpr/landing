f20ca0ef1ea1c20eeae7282439484c58060e187f
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-08 11:16:19 -0700

    Add & style images

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import './Testimonials.scss';
+import onePortrait from '../../assets/one-portrait.jpg';
+import twoPortrait from '../../assets/two-portrait.jpg';
+import threePortrait from '../../assets/three-portrait.jpg';
 
 const Testimonials = () => {
   return (
     <div className='Testimonials'>
       <h3>What our customers are saying...</h3>
+      <div className='Testimonials__testimonial'>
+        <div className='Testimonials__imageWrapper'>
+          <img
+            className='Testimonials__image'
+            alt='Happy Face!'
+            src={onePortrait}
+          />
+        </div>
+      </div>
+
+      <div className='Testimonials__testimonial'>
+        <div className='Testimonials__imageWrapper'>
+          <img
+            className='Testimonials__image'
+            alt='Happy Face!'
+            src={twoPortrait}
+          />
+        </div>
+      </div>
+
+      <div className='Testimonials__testimonial'>
+        <div className='Testimonials__imageWrapper'>
+          <img
+            className='Testimonials__image'
+            alt='Happy Face!'
+            src={threePortrait}
+          />
+        </div>
+      </div>
     </div>
   );
 };
