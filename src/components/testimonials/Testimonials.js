cffd7b0d857cef16ec96147ad04c8be11ead361d
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-08 11:42:06 -0700

    Reduce image size

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -8,40 +8,42 @@ const Testimonials = () => {
   return (
     <div className='Testimonials'>
       <h3>What our customers are saying...</h3>
-      <div className='Testimonials__testimonial'>
-        <div className='Testimonials__imageWrapper'>
-          <img
-            className='Testimonials__image'
-            alt='Happy Face!'
-            src={onePortrait}
-          />
+      <div className='Testimonials__wrapper'>
+        <div className='Testimonials__testimonial'>
+          <div className='Testimonials__imageWrapper'>
+            <img
+              className='Testimonials__image'
+              alt='Happy Face!'
+              src={onePortrait}
+            />
+          </div>
+          <h5>Mary M.</h5>
+          <p>"This saves me so much time!"</p>
         </div>
-        <h5>Mary M.</h5>
-        <p>"This saves me so much time!"</p>
-      </div>
 
-      <div className='Testimonials__testimonial'>
-        <div className='Testimonials__imageWrapper'>
-          <img
-            className='Testimonials__image'
-            alt='Happy Face!'
-            src={twoPortrait}
-          />
+        <div className='Testimonials__testimonial'>
+          <div className='Testimonials__imageWrapper'>
+            <img
+              className='Testimonials__image'
+              alt='Happy Face!'
+              src={twoPortrait}
+            />
+          </div>
+          <h5>Charlie C.</h5>
+          <p>"This is so useful!"</p>
         </div>
-        <h5>Charlie C.</h5>
-        <p>"This is so useful!"</p>
-      </div>
 
-      <div className='Testimonials__testimonial'>
-        <div className='Testimonials__imageWrapper'>
-          <img
-            className='Testimonials__image'
-            alt='Happy Face!'
-            src={threePortrait}
-          />
+        <div className='Testimonials__testimonial'>
+          <div className='Testimonials__imageWrapper'>
+            <img
+              className='Testimonials__image'
+              alt='Happy Face!'
+              src={threePortrait}
+            />
+          </div>
+          <h5>Elaine E.</h5>
+          <p>"My landing page looks fantastic!"</p>
         </div>
-        <h5>Elaine E.</h5>
-        <p>"My landing page looks fantastic!"</p>
       </div>
     </div>
   );
