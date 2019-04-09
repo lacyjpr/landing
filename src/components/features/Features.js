7633f597a2820f273fb4cfd16c3a3d32d2212f55
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-09 09:22:46 -0700

    Add features array

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -4,6 +4,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDesktop, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
 
+const features = [
+  {
+    key: 1,
+    icon: faDesktop,
+    feat: "It's Responsive",
+  },
+  {
+    key: 2,
+    icon: faLayerGroup,
+    feat: "It's Built With Bootstrap",
+  },
+  {
+    key: 3,
+    icon: faCheckCircle,
+    feat: 'It Looks Great!',
+  },
+];
+
 const Features = () => {
   return (
     <div className='Features'>
