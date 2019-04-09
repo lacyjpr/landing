9d0eb9f798033583f3b874cde295cfe7476a4f34
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-09 10:04:42 -0700

    DRY up Advantages

--- src/components/advantages/Advantages.js
+++ src/components/advantages/Advantages.js
@@ -4,20 +4,20 @@ import './Advantages.scss';
 const advantages = [
   {
     key: 1,
-    image: "'Advantages__image-one'",
-    text: "'Advantages__text-one'",
+    image: 'Advantages__image-one',
+    text: 'Advantages__text-one',
     heading: 'Responsive Architecture',
   },
   {
     key: 2,
-    image: "'Advantages__image-two'",
-    text: "'Advantages__text-two'",
+    image: 'Advantages__image-two',
+    text: 'Advantages__text-two',
     heading: 'New and Improved',
   },
   {
-    key: 1,
-    image: "'Advantages__image-three'",
-    text: "'Advantages__text-three'",
+    key: 3,
+    image: 'Advantages__image-three',
+    text: 'Advantages__text-three',
     heading: 'Responsive Architecture',
   },
 ];
@@ -25,36 +25,20 @@ const advantages = [
 const Advantages = () => {
   return (
     <div className='Advantages'>
-      <div className='Advantages__image-one' />
-      <div className='Advantages__text-one'>
-        <h2>Responsive Architecture</h2>
-        <p>
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-        </p>
-      </div>
-      <div className='Advantages__image-two' />
-      <div className='Advantages__text-two'>
-        <h2>New and Improved!</h2>
-        <p>
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-        </p>
-      </div>
-      <div className='Advantages__image-three' />
-      <div className='Advantages__text-three'>
-        <h2>User Friendly!</h2>
-        <p>
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
-        </p>
-      </div>
+      {advantages.map(advantage => (
+        <>
+          <div className={advantage.image} key={advantage.key} />
+          <div className={advantage.text}>
+            <h2>{advantage.heading}</h2>
+            <p>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
+            </p>
+          </div>
+        </>
+      ))}
     </div>
   );
 };
