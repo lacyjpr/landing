bc9b1927cb829a7e9d2dc9fa3bd77638834ec3d7
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-09 09:00:33 -0700

    DRY up Testimonials

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -4,46 +4,45 @@ import onePortrait from '../../assets/one-portrait.jpg';
 import twoPortrait from '../../assets/two-portrait.jpg';
 import threePortrait from '../../assets/three-portrait.jpg';
 
+const testimonies = [
+  {
+    key: 1,
+    name: 'Mary M.',
+    quote: "'I love working with this!'",
+    image: onePortrait,
+  },
+  {
+    key: 2,
+    name: 'Charlie C.',
+    quote: "'This is so useful & fun to use!'",
+    image: twoPortrait,
+  },
+  {
+    key: 3,
+    name: 'Elaine E.',
+    quote: "'My landing page looks fantastic!'",
+    image: threePortrait,
+  },
+];
+
 const Testimonials = () => {
   return (
     <div className='Testimonials'>
       <h3>What users are saying...</h3>
       <div className='Testimonials__wrapper'>
-        <div className='Testimonials__testimonial'>
-          <div className='Testimonials__imageWrapper'>
-            <img
-              className='Testimonials__image'
-              alt='Happy Face!'
-              src={onePortrait}
-            />
+        {testimonies.map(testimony => (
+          <div className='Testimonials__testimonial' key={testimony.key}>
+            <div className='Testimonials__imageWrapper'>
+              <img
+                className='Testimonials__image'
+                alt='Happy Face!'
+                src={testimony.image}
+              />
+            </div>
+            <h5>{testimony.name}</h5>
+            <p>{testimony.quote}</p>
           </div>
-          <h5>Mary M.</h5>
-          <p>"I love working with this!"</p>
-        </div>
-
-        <div className='Testimonials__testimonial'>
-          <div className='Testimonials__imageWrapper'>
-            <img
-              className='Testimonials__image'
-              alt='Happy Face!'
-              src={twoPortrait}
-            />
-          </div>
-          <h5>Charlie C.</h5>
-          <p>"This is so useful & fun to use!"</p>
-        </div>
-
-        <div className='Testimonials__testimonial'>
-          <div className='Testimonials__imageWrapper'>
-            <img
-              className='Testimonials__image'
-              alt='Happy Face!'
-              src={threePortrait}
-            />
-          </div>
-          <h5>Elaine E.</h5>
-          <p>"My landing page looks fantastic!"</p>
-        </div>
+        ))}
       </div>
     </div>
   );
