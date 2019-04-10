a671e19ced97f7047a0a38fbf930bc1bc21992fd
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-10 16:08:41 -0700

    Add left ul

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,10 +3,23 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer Component</h1>
+    <div className='Footer'>
+      <div className='Footer__left'>
+        <ul className='Footer__left-list'>
+          <li className='Footer__left-item'>About</li>
+          <li className='Footer__left-item'>⋅</li>
+          <li className='Footer__left-item'>Contact</li>
+          <li className='Footer__left-item'>⋅</li>
+          <li className='Footer__left-item'>Terms of Use</li>
+          <li className='Footer__left-item'>⋅</li>
+          <li className='Footer__left-item'>Privacy Policy</li>
+        </ul>
+      </div>
+      <div className='Footer__right' />
     </div>
   );
 };
 
 export default Footer;
+
+//<li class="list-inline-item">⋅</li>
