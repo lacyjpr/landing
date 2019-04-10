bdcfa78f2b2917f4f00673bad409c971cbeeda58
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-10 11:15:31 -0700

    Style Parting component

--- src/components/parting/Parting.js
+++ src/components/parting/Parting.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import blueWater from '../../assets/blue-water.jpg';
 import './Parting.scss';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -7,7 +6,6 @@ import Button from 'react-bootstrap/Button';
 const Parting = () => {
   return (
     <div className='Parting'>
-      <img className='Parting__image' alt='Blue Water' src={blueWater} />
       <h1 className='Parting__text'>Ready to get started? Sign up now!</h1>
       <Form className='Parting__form'>
         <div className='Parting__form-email'>
